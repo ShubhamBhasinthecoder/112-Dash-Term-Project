4ccecd360c57b463c5c584d4522d91401c3b0c22
ShubhamBhasinthecoder <145772732+ShubhamBhasinthecoder@users.noreply.github.com>
2025-07-27 23:51:27 -0400

    Update visual_effects.py

--- visual_effects.py
+++ visual_effects.py
@@ -1,2 +1,39 @@
 #file for the sound effects in the game. temporary addition, may not add this feature
 #replacing this with an additional file for extra visual effects.
+from cmu_graphics import *
+import random
+import math
+
+class VisualEffect:
+    def __init__(self, effectType, x, y, maxLife=30):
+        self.type = effectType
+        self.x = x
+        self.y = y
+        self.life = maxLife
+        self.maxLife = maxLife
+    
+    def update(self):
+        self.life -= 1
+    
+    def isExpired(self):
+        return self.life <= 0
+    
+    def draw(self, cameraOffset, screenWidth):
+        screenX = self.x - cameraOffset
+        
+        
+        if -50 < screenX < screenWidth + 50:
+            alpha = self.life / self.maxLife
+            
+            if self.type == 'jump':
+                size = 20 * alpha
+                drawCircle(screenX, self.y, size, fill='white', border='gray', borderWidth=2)
+            elif self.type == 'coin':
+                drawLabel('♪', screenX, self.y, size=int(20 * alpha), fill='gold')
+            elif self.type == 'death':
+                size = 30 * alpha
+                drawStar(screenX, self.y, size, 8, fill='red')
+            elif self.type == 'powerup':
+                drawLabel('✨', screenX, self.y, size=int(25 * alpha), fill='cyan')
+            elif self.type == 'victory':
+                drawLabel('🎉', screenX, self.y, size=int(30 * alpha), fill='gold')
